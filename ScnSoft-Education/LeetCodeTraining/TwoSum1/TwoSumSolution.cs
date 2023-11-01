ad7b847c379d17019fb1df0205d4b4d324d20e17
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-01 13:29:20 +0300

    Add files via upload

---
+++ ScnSoft-Education/LeetCodeTraining/TwoSum1/TwoSumSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTraining.TwoSum1
+{
+    //https://leetcode.com/problems/two-sum/
+    public class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+
+            }
+            return new int[] { };
+        }
+
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}
