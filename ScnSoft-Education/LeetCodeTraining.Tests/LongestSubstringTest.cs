7a46b06577259609a152e1bc74a18ffafe91f191
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-02 17:03:51 +0300

    Add files via upload

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstringTest.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeTraining.LongestSubstring1;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestSubstringSolutionTest
+    {
+        [Fact]
+        public void LongestSubstring()
+        {
+            //Arrange
+            string s1 = "abcabcbb";
+            int expectedResults1 = 3;
+
+            string s2 = "bbbbb";
+            int expectedResults2 = 1;
+
+            string s3 = "pwwkew";
+            int expectedResults3 = 3;
+
+            //Act
+            int Result1 = SubstringSolution.LengthOfLongestSubstring(s1);
+            int Result2 = SubstringSolution.LengthOfLongestSubstring(s2);
+            int Result3 = SubstringSolution.LengthOfLongestSubstring(s3);
+
+            //Assert
+            Assert.Equal(expectedResults1, Result1);
+            Assert.Equal(expectedResults2, Result2);
+            Assert.Equal(expectedResults3, Result3);
+        }
+    }
+}
