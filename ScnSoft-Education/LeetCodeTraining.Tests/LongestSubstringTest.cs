ce67f3e12ffdb1bccb55abc0d748c6002b15d2f7
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-03 13:11:32 +0300

    Update LongestSubstringTest.cs

--- ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstringTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstringTest.cs
@@ -1,31 +1,17 @@
-﻿using LeetCodeTraining.LongestSubstring1;
+using LeetCodeTraining.LongestSubstring1;
 
 namespace LeetCodeTraining.Tests
 {
     public class LongestSubstringSolutionTest
     {
-        [Fact]
-        public void LongestSubstring()
+        [Theory]
+        [InlineData("abcabcbb",3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void LongestSubstring(string s,int expectedResults)
         {
-            //Arrange
-            string s1 = "abcabcbb";
-            int expectedResults1 = 3;
-
-            string s2 = "bbbbb";
-            int expectedResults2 = 1;
-
-            string s3 = "pwwkew";
-            int expectedResults3 = 3;
-
-            //Act
-            int Result1 = SubstringSolution.LengthOfLongestSubstring(s1);
-            int Result2 = SubstringSolution.LengthOfLongestSubstring(s2);
-            int Result3 = SubstringSolution.LengthOfLongestSubstring(s3);
-
-            //Assert
-            Assert.Equal(expectedResults1, Result1);
-            Assert.Equal(expectedResults2, Result2);
-            Assert.Equal(expectedResults3, Result3);
+            int Result = SubstringSolution.LengthOfLongestSubstring(s);
+            Assert.Equal(expectedResults, Result);
         }
     }
 }
