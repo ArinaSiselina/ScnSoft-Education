488791df59ca7fb1a64c200bd3e8824d16085558
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-08 12:18:06 +0300

    Add files via upload

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/IsPalindromeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.IsPalindrome1;
+
+namespace LeetCodeTraining.Tests
+{
+    public class IsPalindromeSolutionTest
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        public void LongestSubstring(int x, bool expectedResult)
+        {
+            //Act
+            bool Result = PalindromeSolution.IsPalindrome(x);
+
+            //Assert
+            Assert.Equal(expectedResult, Result);
+        }
+    }
+}
