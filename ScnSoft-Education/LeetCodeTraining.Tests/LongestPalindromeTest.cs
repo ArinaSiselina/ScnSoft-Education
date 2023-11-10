b3a601ff7ec9befa0c0d71ea52e17ce072adcd91
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-10 19:42:51 +0300

    Add files via upload

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestPalindromeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.LongestPalindrome;
+
+namespace LeetCodeTraining.Tests
+{
+    public class longestPalindromeSolutionTest
+    {
+        [Theory]
+        [InlineData("babad", "aba")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("fghjklkj", "jklkj")]
+        public void LongestPalindrome(string s, string expectedResult)
+        {
+            //Act
+            string Result = LongestPalindromeSolution.LongestPalindrome(s);
+
+            //Assert
+            Assert.Equal(expectedResult, Result);
+        }
+    }
+}
