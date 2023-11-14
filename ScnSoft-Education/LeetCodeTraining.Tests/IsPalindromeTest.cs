ad0a705f857aaba50efa815f3330b1da6a4c6c63
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-14 11:28:13 +0300

    Update IsPalindromeTest.cs

--- ScnSoft-Education/LeetCodeTraining.Tests/IsPalindromeTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/IsPalindromeTest.cs
@@ -8,13 +8,13 @@ namespace LeetCodeTraining.Tests
         [InlineData(121, true)]
         [InlineData(-121, false)]
         [InlineData(10, false)]
-        public void LongestSubstring(int x, bool expectedResult)
+        public void IsPalindrome(int x, bool expectedResult)
         {
             //Act
-            bool Result = PalindromeSolution.IsPalindrome(x);
+            bool result = PalindromeSolution.IsPalindrome(x);
 
             //Assert
-            Assert.Equal(expectedResult, Result);
+            Assert.Equal(expectedResult, result);
         }
     }
 }
