f8fc86f3ccae146c1a72f5572dae102259f29c95
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-15 19:47:33 +0300

    #4:The solution to the longest palindrome problem (#4)
    * Add files via upload
    
    * Add files via upload
    
    * Update LongestPalindromeTest.cs
    
    * Fix Longest Palindrome problem
    
    * Change link to the LeetCode problem
    
    * Update LongestSubstringSolution.cs

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestPalindromeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.LongestPalindrome;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestPalindromeSolutionTest
+    {
+        [Theory]
+        [InlineData("babad", "aba")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("fghjklkj", "jklkj")]
+        public void LongestPalindrome(string s, string expectedResult)
+        {
+            //Act
+            string result = LongestPalindromeSolution.LongestPalindrome(s);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
