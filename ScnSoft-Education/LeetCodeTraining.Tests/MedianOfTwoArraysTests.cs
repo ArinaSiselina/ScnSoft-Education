9b1e42110af8d7e07eef0e674dd8c0f71b39b715
ArinaSiselins <asiselina@scnsoft.com>
2023-11-16 15:00:10 +0300

    Fix MedianOfTwoSortedArrays problem solution

--- ScnSoft-Education/LeetCodeTraining.Tests/MedianOfTwoArraysTests.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/MedianOfTwoArraysTests.cs
@@ -8,7 +8,7 @@ namespace LeetCodeTraining.Tests
         [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2.00000)]
         [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.50000)]
         [InlineData(new int[] { 1, 3 }, new int[] { 5, 8 }, 4.00000)]
-        public void MedianOfTwoArrays(int[] nums1, int[] nums2, double expectedResult)
+        public void MedianOfTwoSortedArrays(int[] nums1, int[] nums2, double expectedResult)
         {
             //Act
             double result = MedianOfTwoArraysSolution.FindMedianSortedArrays(nums1, nums2);
