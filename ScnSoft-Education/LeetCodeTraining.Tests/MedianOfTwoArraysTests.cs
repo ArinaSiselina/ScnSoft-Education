659c10e29cc612116a3aabc65a1ab8c5c51d8858
ArinaSiselins <asiselina@scnsoft.com>
2023-11-16 11:47:42 +0300

    Fix MedianOfTwoSortedArrays problem

--- ScnSoft-Education/LeetCodeTraining.Tests/MedianOfTwoArraysTests.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/MedianOfTwoArraysTests.cs
@@ -5,7 +5,7 @@ namespace LeetCodeTraining.Tests
     public class MedianOfTwoArraysSolutionTest
     {
         [Theory]
-        [InlineData(new int[]{1, 3}, new int[]{2}, 2.00000)]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2.00000)]
         [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.50000)]
         [InlineData(new int[] { 1, 3 }, new int[] { 5, 8 }, 4.00000)]
         public void MedianOfTwoArrays(int[] nums1, int[] nums2, double expectedResult)
---
+++ ScnSoft-Education/LeetCodeTraining/MedianOfTwoarrays5/MedianOfTwoArraysSolution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeTraining.MedianOfTwoArrays
+{
+    //https://leetcode.com/problems/median-of-two-sorted-arrays/
+    public class MedianOfTwoArraysSolution
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int n = nums1.Length + nums2.Length;
+            int[] merged = new int[n];
+            int i = 0, j = 0, k = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] < nums2[j])
+                {
+                    merged[k++] = nums1[i++];
+                }
+                else
+                {
+                    merged[k++] = nums2[j++];
+                }
+            }
+
+            while (i < nums1.Length)
+            {
+                merged[k++] = nums1[i++];
+            }
+
+            while (j < nums2.Length)
+            {
+                merged[k++] = nums2[j++];
+            }
+
+            if (n % 2 == 0)
+            {
+                return (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
+            }
+            return merged[n / 2];
+        }
+    }
+}
