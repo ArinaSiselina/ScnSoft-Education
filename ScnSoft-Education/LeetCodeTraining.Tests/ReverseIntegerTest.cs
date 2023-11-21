ba26200437cec5b1bcf3b601d56f95a140d0936e
ArinaSiselins <asiselina@scnsoft.com>
2023-11-21 18:36:37 +0300

    Add ReverseInteger problem solution and tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ReverseIntegerTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.ReverseInteger;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ReverseIntegerSolutionTest
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        public void ReverseInteger(int x, int expectedResult)
+        {
+            //Act
+            int Result = ReverseIntegerSolution.Reverse(x);
+
+            //Assert
+            Assert.Equal(expectedResult, Result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ReverseInteger7/ReverseIntegerSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTraining.ReverseInteger
+{
+    //https://leetcode.com/problems/reverse-integer/description/
+    public class ReverseIntegerSolution
+    {
+        public static int Reverse(int x)
+        {
+            int reversedX = 0;
+            while (x != 0)
+            {
+                int digit = x % 10;
+                x /= 10;
+                if (reversedX > int.MaxValue / 10 || (reversedX == int.MaxValue / 10 && digit > 7))
+                {
+                    return 0;
+                }
+                if (reversedX < int.MinValue / 10 || (reversedX == int.MinValue / 10 && digit < -8))
+                {
+                    return 0;
+                }
+                reversedX = reversedX * 10 + digit;
+            }
+            return reversedX;
+        }
+    }
+}
