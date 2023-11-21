b2950372eafd268652f2f7c5d2fd66e3b1d22324
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-11-21 11:23:04 +0300

    #6: The solution to the zigzag conversion problem (#6)
    * Add solution and test files
    
    * Add files via upload
    
    * Vertical align

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ZigzagConversionTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.ZigzagConversion;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ZigzagConversionSolutionTest
+    {
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [InlineData("A", 1, "A")]
+        public void ZigzagConversion(string s, int numRows, string expectedResult)
+        {
+            //Act
+            string result = ZigzagConversionSolution.Convert(s, numRows);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
