bc2b513bba9128313e4227daa3f41dd436f2177a
ArinaSiselins <asiselina@scnsoft.com>
2023-11-27 15:30:49 +0300

    Add StringToIntegerAtoi problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/StringToIntegerAtoiTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.StringToIntegerAtoi;
+
+namespace LeetCodeTraining.Tests
+{
+    public class StringToIntegerAtoiSolutionTest
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData("   -42", -42)]
+        [InlineData("4193 with words", 4193)]
+        public void StringToIntegerAtoi(string s, int expectedResult)
+        {
+            //Act
+            int Result = StringToIntegerAtoiSolution.MyAtoi(s);
+
+            //Assert
+            Assert.Equal(expectedResult, Result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/StringToIntegerAtoi8/StringToIntegerAtoiSolution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeTraining.StringToIntegerAtoi
+{
+    // https://leetcode.com/problems/string-to-integer-atoi/
+    public class StringToIntegerAtoiSolution
+    {
+        public static int MyAtoi(string s)
+        {
+            s = s.TrimStart();
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            bool isNegative = false;
+            int index = 0;
+
+            if (s[0] == '-' || s[0] == '+')
+            {
+                isNegative = s[0] == '-';
+                index++;
+            }
+
+            long result = 0;
+            while (index < s.Length && Char.IsDigit(s[index]))
+            {
+                result = result * 10 + (int)Char.GetNumericValue(s[index]);
+                if (isNegative && -result < int.MinValue) return int.MinValue;
+                if (!isNegative && result > int.MaxValue) return int.MaxValue;
+                index++;
+            }
+            return isNegative ? (int)-result : (int)result;
+        }
+    }
+}
