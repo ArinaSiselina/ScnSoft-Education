6ac17e610195f30f0bcff5fb0170f0471ffeb80e
ArinaSiselins <asiselina@scnsoft.com>
2023-11-29 14:20:10 +0300

    Add AddTwoNumbers problemSolution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/AddTwoNumbersTest.cs
@@ -0,0 +1,60 @@
+﻿using LeetCodeTraining.AddTwoNumbers;
+
+namespace LeetCodeTraining.Tests
+{
+    public class AddTwoNumbersSolutionTest
+    {
+
+        [Fact]
+        public void AddTwoNumbers1()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+            // Act
+            ListNode result = AddTwoNumbersSolution.AddTwoNumbers(l1, l2);
+
+            // Assert
+            Assert.Equal(7, result.val);
+            Assert.Equal(0, result.next.val);
+            Assert.Equal(8, result.next.next.val);
+        }
+
+        [Fact]
+        public void AddTwoNumbers2()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(0);
+            ListNode l2 = new ListNode(0);
+
+            // Act
+            ListNode result = AddTwoNumbersSolution.AddTwoNumbers(l1, l2);
+
+            // Assert
+            Assert.Equal(0, result.val);
+        }
+
+        [Fact]
+        public void AddTwoNumbers3()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+            ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+
+            // Act
+            ListNode result = AddTwoNumbersSolution.AddTwoNumbers(l1, l2);
+
+            // Assert [8,9,9,9,0,0,0,1]
+            Assert.Equal(8, result.val);
+            Assert.Equal(9, result.next.val);
+            Assert.Equal(9, result.next.next.val);
+            Assert.Equal(9, result.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.next.next.val);
+            Assert.Equal(1, result.next.next.next.next.next.next.next.val);
+        }
+
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/AddTwoNumbers9/AddTwoNumbersSolution.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LeetCodeTraining.AddTwoNumbers
+{
+    // https://leetcode.com/problems/string-to-integer-atoi/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class AddTwoNumbersSolution
+    { 
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode one = l1, two = l2, curr = result;
+            int remember = 0;
+
+            while (one != null || two != null)
+            {
+                int x = (one != null) ? one.val : 0;
+                int y = (two != null) ? two.val : 0;
+
+                int sum = remember + x + y;
+                remember = sum / 10;
+
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+
+                if (one != null) one = one.next;
+                if (two != null) two = two.next;
+            }
+
+            if (remember > 0)
+            {
+                curr.next = new ListNode(remember);
+            }
+
+            return result.next;
+        }
+    }
+}
