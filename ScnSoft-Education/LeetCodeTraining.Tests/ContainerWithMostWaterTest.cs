0a1241de7c83ec0e891aac71260c9be90c77e451
ArinaSiselins <asiselina@scnsoft.com>
2023-12-01 12:22:13 +0300

    Add ContainerWithMostWater problem Solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ContainerWithMostWaterTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.ContainerWithMostWater;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ContainerWithMostWaterSolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 4, 3, 2, 1, 4 }, 16)]
+        public void MaxArea(int[] height, int expectedResult)
+        {
+            //Act
+            int result = ContainerWithMostWaterSolution.MaxArea(height);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ContainerWithMostWater-11/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LeetCodeTraining.ContainerWithMostWater
+
+{
+    // https://leetcode.com/problems/container-with-most-water/
+    public class ContainerWithMostWaterSolution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int currentArea = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(maxArea, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
