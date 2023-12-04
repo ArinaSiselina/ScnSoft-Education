a4e2af53ca35e89258e9d3774fd55f55b71ee563
ArinaSiselins <asiselina@scnsoft.com>
2023-12-04 14:54:03 +0300

    Add ThreeSumClosest problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ThreeSumClosestTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeTraining.ThreeSumClosest;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ThreeSumClosestSolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { -1, 2, 1, -4 }, 1, 2)]
+        [InlineData(new int[] { 0, 0, 0 }, 1, 0)]
+        public void ThreeSumClosest(int[] nums, int target, int expectedResult)
+        {
+            //Act
+            int result = ThreeSumClosestSolution.ThreeSumClosest(nums, target);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/3SumClosest-16/3SumClosestSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeTraining.ThreeSumClosest
+{
+    //https://leetcode.com/problems/3sum-closest/
+    public class ThreeSumClosestSolution
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int closestSum = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int left = i + 1, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int currSum = nums[i] + nums[left] + nums[right];
+                    if (Math.Abs(currSum - target) < Math.Abs(closestSum - target))
+                    {
+                        closestSum = currSum;
+                    }
+
+                    if (currSum < target)
+                    {
+                        left++;
+                    }
+                    else if (currSum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        return closestSum;
+                    }
+                }
+            }
+            return closestSum;
+        }
+    }
+}
