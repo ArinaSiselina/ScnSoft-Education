a9aa42e1e3f62aa77d742112fdb5522e50012cf0
ArinaSiselins <asiselina@scnsoft.com>
2023-12-04 14:45:00 +0300

    Add 3Sum problem solution

--- ScnSoft-Education/LeetCodeTraining.Tests/3SumTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/3SumTest.cs
@@ -5,18 +5,42 @@ namespace LeetCodeTraining.Tests
     public class ThreeSumSolutionTest
     {
         [Theory]
-        [InlineData(new int[] { -1, 0, 1, 2, -1, -4 }, new int[][] { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } })]
-        [InlineData(new int[] { 0, 1, 1 }, false)]
-        [InlineData(new int[] { 0, 0, 0 }, false)]
-        public void ThreeSum(int[] nums, IList<IList<int>> expectedResult)
+        [InlineData("-1,0,1,2,-1,-4", "[-1,-1,2],[-1,0,1]")]
+        [InlineData("0,1,1", "")]
+        [InlineData("0,0,0", "0,0,0")]
+        public void ThreeSum(string inputStr, string expectedStr)
         {
+            int[] input = inputStr.Split(',').Select(int.Parse).ToArray();
+
+            List<List<int>> expected = new List<List<int>>();
+            if (!string.IsNullOrEmpty(expectedStr))
+            {
+                expected = expectedStr.Split(']')
+                                      .Where(s => !string.IsNullOrWhiteSpace(s) && s != ",")
+                                      .Select(s => s.Trim('[', ',')
+                                      .Split(',')
+                                      .Select(int.Parse)
+                                      .ToList())
+                                      .ToList();
+            }
+
             //Act
-            IList<IList<int>> result = ThreeSumSolution.ThreeSum(nums);
+            IList<IList<int>> result = ThreeSumSolution.ThreeSum(input);
+
+            List<List<int>> resultList = new List<List<int>>();
+            foreach (var list in result)
+            {
+                resultList.Add(list.OrderBy(x => x).ToList());
+            }
 
             //Assert
-            Assert.Equal(expectedResult, result);
+            Assert.Equal(expected.OrderBy(list => string.Join(",", list)), resultList.OrderBy(list => string.Join(",", list)));
         }
     }
 }
+
+
+
+
 
 
