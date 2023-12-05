1c42c17ce990f7cc475e7bf399d68a725f4c6189
ArinaSiselins <asiselina@scnsoft.com>
2023-12-05 16:02:09 +0300

    Edit 3Sum problem solution tests

--- ScnSoft-Education/LeetCodeTraining.Tests/3SumTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/3SumTest.cs
@@ -4,7 +4,7 @@ namespace LeetCodeTraining.Tests
 {
     public class ThreeSumSolutionTest
     {
-        [Theory]
+        /*[Theory]
         [InlineData("-1,0,1,2,-1,-4", "[-1,-1,2],[-1,0,1]")]
         [InlineData("0,1,1", "")]
         [InlineData("0,0,0", "0,0,0")]
@@ -35,6 +35,54 @@ namespace LeetCodeTraining.Tests
 
             //Assert
             Assert.Equal(expected.OrderBy(list => string.Join(",", list)), resultList.OrderBy(list => string.Join(",", list)));
+        }*/
+     
+        public class ThreeSumSolutionTests
+        {
+            [Theory]
+            [MemberData(nameof(Data))]
+            public void ThreeSumTest(int[] nums, IList<IList<int>> expectedResult)
+            {
+                // Arrange
+                var solution = new ThreeSumSolution();
+
+                // Act
+                var result = ThreeSumSolution.ThreeSum(nums);
+
+                // Assert
+                Assert.Equal(expectedResult.Count, result.Count);
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Assert.Equal(expectedResult[i], result[i]);
+                }
+            }
+
+            public static IEnumerable<object[]> Data =>
+                new List<object[]>
+                {
+                new object[]
+                {
+                    new int[] { -1, 0, 1, 2, -1, -4 },
+                    new List<IList<int>>
+                    {
+                        new List<int> { -1, -1, 2 },
+                        new List<int> { -1, 0, 1 }
+                    }
+                },
+                new object[]
+                {
+                    new int[] { 0, 1, 1 },
+                    new List<IList<int>> { }
+                },
+                new object[]
+                {
+                    new int[] { 0, 0, 0 },
+                    new List<IList<int>> { new List<int> { 0, 0, 0 } }
+                }
+                // Add more test cases here as needed
+                };
+
+
         }
     }
 }
