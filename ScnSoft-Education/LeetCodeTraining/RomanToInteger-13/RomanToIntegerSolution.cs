312b39841db530946c6043daaf3447ae39fd9f52
ArinaSiselins <asiselina@scnsoft.com>
2023-12-05 15:12:32 +0300

    Edit vertical alignment

--- ScnSoft-Education/LeetCodeTraining/RomanToInteger-13/RomanToIntegerSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/RomanToInteger-13/RomanToIntegerSolution.cs
@@ -10,8 +10,8 @@
                 {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
                 {'C', 100}, {'D', 500}, {'M', 1000}
             };
-
             int result = 0;
+
             for (int i = 0; i < s.Length; i++)
             {
                 if (i < s.Length - 1 && romanMap[s[i]] < romanMap[s[i + 1]])
