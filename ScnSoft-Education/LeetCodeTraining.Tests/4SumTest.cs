a1241f7e5d4dfb05cecd5a97dba53750f11ca00f
ArinaSiselins <asiselina@scnsoft.com>
2023-12-06 18:59:58 +0300

    Add 4Sum problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/4SumTest.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeTraining.FourSum;
+
+namespace LeetCodeTraining.Tests
+{
+    public class FourSumSolutionTests
+    {
+        public static IEnumerable<object[]> FourSumTestCases()
+        {
+            yield return new object[]
+            {
+                new int[] { 1, 0, -1, 0, -2, 2 }, 0,
+                new int[][] { new int[] { -2, -1, 1, 2 }, new int[] { -2, 0, 0, 2 }, new int[] { -1, 0, 0, 1 } }
+            };
+
+            yield return new object[]
+            {
+                new int[] { 2, 2, 2, 2, 2 }, 8,
+                new int[][] { new int[] { 2, 2, 2, 2 } }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(FourSumTestCases))]
+        public void FourSum_ShouldReturnCorrectResult(int[] nums, int target, int[][] expected)
+        {
+            // Act
+            var result = FourSumSolution.FourSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected.Length, result.Count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i].Length, result[i].Count);
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.Equal(expected[i][j], result[i][j]);
+                }
+            }
+        }
+    }
+}
