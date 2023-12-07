02aa6796e1eaef72b08114ceb6d3320b9ef2b14b
ArinaSiselins <asiselina@scnsoft.com>
2023-12-07 14:06:40 +0300

    Edit 4Sum problem solution

--- ScnSoft-Education/LeetCodeTraining.Tests/4SumTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/4SumTest.cs
@@ -8,34 +8,33 @@ namespace LeetCodeTraining.Tests
         {
             yield return new object[]
             {
-                new int[] { 1, 0, -1, 0, -2, 2 }, 0,
-                new int[][] { new int[] { -2, -1, 1, 2 }, new int[] { -2, 0, 0, 2 }, new int[] { -1, 0, 0, 1 } }
+                new int[] { 1, 0, -1, 0, -2, 2 },
+                0,
+                new List<IList<int>>
+                {
+                    new List<int> { -2, -1, 1, 2 },
+                    new List<int> { -2, 0, 0, 2 },
+                    new List<int> { -1, 0, 0, 1 }
+                }
             };
 
             yield return new object[]
             {
-                new int[] { 2, 2, 2, 2, 2 }, 8,
-                new int[][] { new int[] { 2, 2, 2, 2 } }
+                new int[] { 2, 2, 2, 2, 2 },
+                8,
+                new List<IList<int>> { new List<int> { 2, 2, 2, 2 } }
             };
         }
 
         [Theory]
         [MemberData(nameof(FourSumTestCases))]
-        public void FourSum_ShouldReturnCorrectResult(int[] nums, int target, int[][] expected)
+        public void FourSum(int[] nums, int target, List<IList<int>> expected)
         {
             // Act
             var result = FourSumSolution.FourSum(nums, target);
 
             // Assert
-            Assert.Equal(expected.Length, result.Count);
-            for (int i = 0; i < expected.Length; i++)
-            {
-                Assert.Equal(expected[i].Length, result[i].Count);
-                for (int j = 0; j < expected[i].Length; j++)
-                {
-                    Assert.Equal(expected[i][j], result[i][j]);
-                }
-            }
+            Assert.Equal(expected, result);
         }
     }
 }
--- ScnSoft-Education/LeetCodeTraining/4Sum-18/4SumSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/4Sum-18/4SumSolution.cs
@@ -15,23 +15,40 @@
 
             for (int i = 0; i < nums.Length - 3; i++)
             {
-                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                if (i > 0 && nums[i] == nums[i - 1]) 
+                { 
+                    continue;
+                }
+                    
                 for (int j = i + 1; j < nums.Length - 2; j++)
                 {
-                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    if (j > i + 1 && nums[j] == nums[j - 1])
+                    {
+                        continue;
+                    }
+                     
                     for (int k = j + 1; k < nums.Length - 1; k++)
                     {
-                        if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+                        if (k > j + 1 && nums[k] == nums[k - 1])
+                        {
+                            continue;
+                        }
+                            
                         int left = k + 1;
 
                         while (left < nums.Length)
                         {
                             long sum = (long)nums[i] + nums[j] + nums[k] + nums[left];
+
                             if (sum == target)
                             {
                                 result.Add(new List<int> { nums[i], nums[j], nums[k], nums[left] });
-                                while (left < nums.Length - 1 && nums[left] == nums[left + 1]) left++;
+                                while (left < nums.Length - 1 && nums[left] == nums[left + 1])
+                                {
+                                    left++;
+                                }   
                             }
+
                             left++;
                         }
                     }
