e15424d657c833d924a94e466935ae3b1609b898
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-07 13:52:14 +0300

    #15: The solution for 3Sum problem (#14)
    * Add 3Sum problem solution
    
    * Add 3Sum problem solution
    
    * Edit 3Sum problem solution tests
    
    * Edit 3Sum problem solution
    
    * Edit 3Sum problem solution tests
    
    * Dekete extra lines
    
    * Edit 3Sum problem solution tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/3SumTest.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeTraining.ThreeSum;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ThreeSumSolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ThreeSumTest(int[] nums, IList<IList<int>> expectedResult)
+        {
+            // Act
+            var result = ThreeSumSolution.ThreeSum(nums);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new int[] { -1, 0, 1, 2, -1, -4 },
+                new List<IList<int>>
+                {
+                    new List<int> { -1, -1, 2 },
+                    new List<int> { -1, 0, 1 }
+                }
+            },
+            new object[]
+            {
+                new int[] { 0, 1, 1 },
+                new List<IList<int>> { }
+            },
+            new object[]
+            {
+                new int[] { 0, 0, 0 },
+                new List<IList<int>> { new List<int> { 0, 0, 0 } }
+            }
+        };
+    }
+}
