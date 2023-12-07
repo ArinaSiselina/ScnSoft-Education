45e3abcc72212d0d9975d72f5a3fd8e6cc0c5321
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-07 13:53:46 +0300

    #16: The solution for ThreeSumClosest problem  (#15)
    * Add ThreeSumClosest problem solution
    
    * Edit 3SumClosest problem solution

---
+++ ScnSoft-Education/LeetCodeTraining/3SumClosest-16/3SumClosestSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeTraining.ThreeSumClosest
+{
+    //https://leetcode.com/problems/3sum-closest/
+    public class ThreeSumClosestSolution
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            int closestSum = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int left = i + 1, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int currSum = nums[i] + nums[left] + nums[right];
+
+                    if (Math.Abs(currSum - target) < Math.Abs(closestSum - target))
+                    {
+                        closestSum = currSum;
+                    }
+
+                    if (currSum == target)
+                        return closestSum;
+
+                    if (currSum < target)
+                    {
+                        left++;
+                        continue;
+                    }
+
+                    right--;
+                }
+            }
+
+            return closestSum;
+        }
+    }
+}
