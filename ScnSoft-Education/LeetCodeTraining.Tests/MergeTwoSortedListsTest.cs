e1654355628660130e34f679d15180e1e1a72f6c
ArinaSiselins <asiselina@scnsoft.com>
2023-12-11 16:16:44 +0300

    Edit MergeTwoSortedLists problem solution and tests

--- ScnSoft-Education/LeetCodeTraining.Tests/MergeTwoSortedListsTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/MergeTwoSortedListsTest.cs
@@ -4,49 +4,37 @@ namespace LeetCodeTraining.Tests
 {
     public class MergeTwoSortedListsSolutionTests
     {
-        public static ListNode InitializeNode(int[] numbers)
-        {
-            if (numbers.Length == 0)
-                return null;
-
-            ListNode head = new ListNode(numbers[0]);
-            ListNode current = head;
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                current.next = new ListNode(numbers[i]);
-                current = current.next;
-            }
-            return head;
-        }
-
-        public static bool CompareNodes(ListNode node1, ListNode node2)
-        {
-            while (node1 != null && node2 != null)
-            {
-                if (node1.val != node2.val)
-                    return false;
-                node1 = node1.next;
-                node2 = node2.next;
-            }
-            return node1 == null && node2 == null;
-        }
-
         [Theory]
-        [InlineData(new[] { 1, 2, 4 }, new[] { 1, 3, 4 }, new[] { 1, 1, 2, 3, 4, 4 })]
-        [InlineData(new int[] { }, new int[] { }, new int[] { })]
-        [InlineData(new int[] { }, new int[] { 0 }, new[] { 0 })]
-        public void MergeTwoLists(int[] list1, int[] list2, int[] expected)
+        [MemberData(nameof(MergeTestData))]
+        public void MergeTwoLists(ListNode list1, ListNode list2, ListNode expected)
         {
-            // Arrange
-            ListNode l1 = InitializeNode(list1);
-            ListNode l2 = InitializeNode(list2);
-            ListNode expectedNode = InitializeNode(expected);
-
             // Act
-            ListNode resultNode = MergeTwoSortedListsSolution.MergeTwoLists(l1, l2);
+            ListNode resultNode = MergeTwoSortedListsSolution.MergeTwoLists(list1, list2);
 
             // Assert
-            Assert.True(CompareNodes(expectedNode, resultNode));
+            Assert.Equal(expected.val, resultNode.val);
+        }
+
+        public static TheoryData<ListNode, ListNode, ListNode> MergeTestData()
+        {
+            return new TheoryData<ListNode, ListNode, ListNode>
+            {
+                {
+                    new ListNode(new[] { 1, 2, 4 }),
+                    new ListNode(new[] { 1, 3, 4 }),
+                    new ListNode(new[] { 1, 1, 2, 3, 4, 4 })
+                },
+                {
+                    new ListNode(new int[] { }),
+                    new ListNode(new int[] { }),
+                    new ListNode(new int[] { })
+                },
+                {
+                    new ListNode(new int[] { }),
+                    new ListNode(new[] { 0 }),
+                    new ListNode(new[] { 0 })
+                }
+            };
         }
     }
 }
--- ScnSoft-Education/LeetCodeTraining/MergeTwoSortedLists-21/MergeTwoSortedListsSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/MergeTwoSortedLists-21/MergeTwoSortedListsSolution.cs
@@ -1,19 +1,78 @@
 ﻿namespace LeetCodeTraining.MergeTwoSortedLists
-{ 
-    // https://leetcode.com/problems/add-two-numbers/
+{
+    //https://leetcode.com/problems/merge-two-sorted-lists/
     public class ListNode
     {
         public int val;
         public ListNode next;
+
         public ListNode(int val = 0, ListNode next = null)
         {
             this.val = val;
             this.next = next;
         }
+
+        public ListNode(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return;
+            }
+
+            this.val = numbers[0];
+            ListNode current = this;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                current.next = new ListNode(numbers[i]);
+                current = current.next;
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            ListNode other = (ListNode)obj;
+            ListNode currentThis = this;
+            ListNode currentOther = other;
+
+            while (currentThis != null && currentOther != null)
+            {
+                if (currentThis.val != currentOther.val)
+                {
+                    return false;
+                }
+                currentThis = currentThis.next;
+                currentOther = currentOther.next;
+            }
+
+            return currentThis == null && currentOther == null;
+        }
+
+        /*public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                ListNode current = this;
+
+                while (current != null)
+                {
+                    hash = hash * 23 + current.val.GetHashCode();
+                    current = current.next;
+                }
+
+                return hash;
+            }
+        }*/
     }
 
     public class MergeTwoSortedListsSolution
-{
+    {
         public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
             ListNode result = new ListNode();
@@ -31,17 +90,11 @@
                     current.next = list2;
                     list2 = list2.next;
                 }
+
                 current = current.next;
             }
 
-            if (list1 != null)
-            {
-                current.next = list1;
-            }
-            else
-            {
-                current.next = list2;
-            }
+            current.next = (list1 != null) ? list1 : list2;
 
             return result.next;
         }
