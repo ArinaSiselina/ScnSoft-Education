112e8c6d54fe445860239e1e19602531c024db8d
ArinaSiselins <asiselina@scnsoft.com>
2023-12-12 13:41:22 +0300

    Add ReverseNodesInKGroup problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ReverseNodesInKGroupTest.cs
@@ -0,0 +1,51 @@
+﻿using LeetCodeTraining.ReverseNodesInKGroup;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ReverseNodesInKGroupSolutionTest
+    {
+        private ListNode InitializeNode(int[] numbers)
+        {
+            ListNode head = new ListNode(numbers[0]);
+            ListNode current = head;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                current.next = new ListNode(numbers[i]);
+                current = current.next;
+            }
+
+            return head;
+        }
+
+        private string DisplayNode(ListNode node)
+        {
+            ListNode current = node;
+            string result = "";
+
+            while (current != null)
+            {
+                result += current.val;
+                current = current.next;
+            }
+
+            return result;
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 2, "21435")]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 3, "32145")]
+        public void ReverseNodesInKGroup(int[] list1, int k, string expectedResult)
+        {
+            //Arrange
+            ListNode l1 = InitializeNode(list1);
+
+            //Act
+            ListNode resultNode = ReverseNodesInKGroupSolution.ReverseKGroup(l1, k);
+            string result = DisplayNode(resultNode);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
