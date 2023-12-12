ae6ccf06a48d491d5b7a5be6287088b259c9e032
ArinaSiselins <asiselina@scnsoft.com>
2023-12-12 12:16:45 +0300

    Add SwapNodesInPairs problem solution

---
+++ ScnSoft-Education/LeetCodeTraining/SwapNodesInPairs-24/SwapNodesInPairsSolution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeTraining.SwapNodesInPairs
+{
+    //https://leetcode.com/problems/swap-nodes-in-pairs/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class SwapNodesInPairsSolution
+    {
+        public static ListNode SwapPairs(ListNode head)
+        {
+            ListNode result = new ListNode(0);
+            result.next = head;
+            ListNode current = result;
+
+            while (current.next != null && current.next.next != null)
+            {
+                ListNode firstNode = current.next;
+                ListNode secondNode = current.next.next;
+
+                firstNode.next = secondNode.next;
+                current.next = secondNode;
+                current.next.next = firstNode;
+
+                current = current.next.next;
+            }
+
+            return result.next;
+        }
+    }
+}
