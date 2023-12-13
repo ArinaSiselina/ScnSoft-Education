372ebcff898caf6e0a4d9c7cbd25304f85137ae0
ArinaSiselins <asiselina@scnsoft.com>
2023-12-13 12:20:14 +0300

    Add RemoveDuplicatesFromSortedArray problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeTraining.RemoveDuplicatesFromSortedArray;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RemoveDuplicatesFromSortedArraySolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void RemoveDuplicatesFromSortedArray(int[] nums, int expectedResult)
+        {
+            //Act
+            int result = RemoveDuplicatesFromSortedArraySolution.RemoveDuplicates(nums);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
