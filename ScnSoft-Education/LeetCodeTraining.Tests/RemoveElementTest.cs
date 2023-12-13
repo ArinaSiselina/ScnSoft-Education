4cbd3d82cab7546247a4d2d7ee01b501ff7a9ca8
ArinaSiselins <asiselina@scnsoft.com>
2023-12-13 12:29:09 +0300

    Add RemoveElement problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveElementTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeTraining.RemoveElement;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RemoveElementSolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2)]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5)]
+        public void RemoveElement(int[] nums, int val, int expectedResult)
+        {
+            //Act
+            int result = RemoveElementSolution.RemoveElement(nums, val);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/RemoveElement-27/RemoveElementSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeTraining.RemoveElement
+{
+    //https://leetcode.com/problems/remove-element/
+    public class RemoveElementSolution
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    int temp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = temp;
+                    k++;
+                }
+            }
+
+            return k;
+        }
+    }
+}
