aa5548f67e0dd140704fcd52e211346c37b34d8d
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-18 12:24:36 +0300

    #23: The solution for MergeKSortedLists problem (#22)
    * Add MergeKSortedLists problem solution
    
    * Add tests and screenshot
    
    * Edit MergeKSortedLists problem solution and tests
    
    * Edit solution
    
    * Edit MergeKSortedLists tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/MergeKSortedListsTest.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeTraining.MergeKSortedLists;
+
+namespace LeetCodeTraining.Tests
+{
+    public class MergeKSortedListsSolutionTest
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void MergeKLists(ListNode[] input, ListNode expected)
+        {
+            // Act
+            ListNode result = MergeKSortedListsSolution.MergeKLists(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static TheoryData<ListNode[], ListNode> TestData =>
+            new TheoryData<ListNode[], ListNode>
+            {
+                {
+                    new ListNode[]
+                    {
+                        new ListNode(new[] { 1, 4, 5 }),
+                        new ListNode(new[] { 1, 3, 4 }),
+                        new ListNode(new[] { 2, 6 })
+                    },
+                    new ListNode(new[] { 1, 1, 2, 3, 4, 4, 5, 6 })
+                },
+                { new ListNode[] { }, null},
+                { new ListNode[] { new ListNode() }, new ListNode()}
+            };
+    }
+}
+
+
