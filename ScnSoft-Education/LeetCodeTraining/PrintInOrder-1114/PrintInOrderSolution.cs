7c68283051e974ae9469855b4f69d5533fa8bff7
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-19 15:11:59 +0300

    Add PrintInOrder problem solution (#32)

---
+++ ScnSoft-Education/LeetCodeTraining/PrintInOrder-1114/PrintInOrderSolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+
+namespace LeetCodeTraining.PrintInOrder
+{
+    //https://leetcode.com/problems/print-in-order/
+    public class Foo
+    {
+
+        private ManualResetEvent firstEvent = new ManualResetEvent(false);
+        private ManualResetEvent secondEvent = new ManualResetEvent(false);
+
+        public Foo()
+        {
+
+        }
+
+        public void First(Action printFirst)
+        {
+            printFirst();
+            firstEvent.Set();
+        }
+
+        public void Second(Action printSecond)
+        {
+            firstEvent.WaitOne();
+            printSecond();
+            secondEvent.Set();
+        }
+
+        public void Third(Action printThird)
+        {
+            secondEvent.WaitOne();
+            printThird();
+        }
+    }
+}
