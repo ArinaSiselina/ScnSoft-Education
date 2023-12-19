1097ffeac9705ddcd73136005ce8387e49f2954d
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-19 15:10:08 +0300

    #10: The solution for RegularExpressionMatching problem (#30)
    * Add RegularExpressionMatching solution
    
    * Edit RegularExpressionMatching problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RegularExpressionMatchingTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.RegularExpressionMatching;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RegularExpressionMatchingSolutionTest
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("ab", ".*", true)]
+        public void IsMatch(string s, string p, bool expectedResult)
+        {
+            //Act
+            bool result = RegularExpressionMatchingSolution.IsMatch(s,p);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
