14307b95ed5806fe520761bcb90ee4bc5a61b6b7
ArinaSiselins <asiselina@scnsoft.com>
2023-12-19 11:05:05 +0300

    Add SortColors problem solution

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/SortColorsTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeTraining.SortColors;
+
+namespace LeetCodeTraining.Tests
+{
+    public class SortColorsSolutionTest
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 0, 2, 1, 1, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+        [InlineData(new int[] { 2, 0, 1 }, new int[] { 0, 1, 2 })]
+        public void SortColors(int[] input, int[] expected)
+        {
+            SortColorsSolution.SortColors(input);
+            Assert.Equal(expected, input);
+        }
+    }
+}
