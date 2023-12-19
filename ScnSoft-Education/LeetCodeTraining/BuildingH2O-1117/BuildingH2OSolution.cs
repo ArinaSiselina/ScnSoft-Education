6f0a3ed3ae39dfb8b789892d40b5131b71a0e061
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-19 15:12:34 +0300

    Add BuildingH2O problem solution (#33)

---
+++ ScnSoft-Education/LeetCodeTraining/BuildingH2O-1117/BuildingH2OSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeTraining.BuildingH2O
+{
+    //https://leetcode.com/problems/building-h2o/
+    public class H2O
+    {
+        private SemaphoreSlim semaphoreO = new(0);
+        private SemaphoreSlim semaphoreH = new(1);
+        private int hcount = 0;
+
+        public H2O()
+        {
+
+        }
+
+        public void Hydrogen(Action releaseHydrogen)
+        {
+            semaphoreH.Wait();
+            hcount += 1;
+
+            releaseHydrogen();
+
+            if (hcount % 2 == 0)
+            {
+                semaphoreO.Release();
+            }
+            else
+            {
+                semaphoreH.Release();
+            }
+
+        }
+
+        public void Oxygen(Action releaseOxygen)
+        {
+            semaphoreO.Wait();
+            releaseOxygen();
+            semaphoreH.Release();
+        }
+    }
+}
