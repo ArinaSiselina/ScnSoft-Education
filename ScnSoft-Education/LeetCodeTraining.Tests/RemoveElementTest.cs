500edadb09bc2cd2fda2016190365a3633dc6dd7
ArinaSiselins <asiselina@scnsoft.com>
2023-12-19 10:46:53 +0300

    Edit RemoveElement problem solution tests

--- ScnSoft-Education/LeetCodeTraining.Tests/RemoveElementTest.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveElementTest.cs
@@ -5,19 +5,24 @@ namespace LeetCodeTraining.Tests
     public class RemoveElementSolutionTest
     {
         [Theory]
-        [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 }, 2)]
-        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 3, 0, 4 }, 5)]
-        public void RemoveElement(int[] nums, int val, int expectedLength, int[] expectedArray, int expectedUniqueCount)
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 3, 0, 4 })]
+        public void RemoveElement(int[] nums, int val, int expectedLength, int[] expectedArray)
         {
-            //Act
+            // Act
             int resultLength = RemoveElementSolution.RemoveElement(nums, val);
 
-            //Assert
+            // Assert
             Assert.Equal(expectedLength, resultLength);
 
             for (int i = 0; i < resultLength; i++)
             {
                 Assert.NotEqual(val, nums[i]);
+
+                if (i < expectedArray.Length)
+                {
+                    Assert.Equal(expectedArray[i], nums[i]);
+                }
             }
         }
     }
