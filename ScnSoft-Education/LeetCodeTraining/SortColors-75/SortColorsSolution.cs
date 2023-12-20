87baf0a2bbeafe90de9868b7fd802598fec52235
ArinaSiselina <149481000+ArinaSiselina@users.noreply.github.com>
2023-12-20 20:15:35 +0300

    #75: The solution for SortColors problem (#34)
    * Add SortColors problem solution
    
    * Add swap function

---
+++ ScnSoft-Education/LeetCodeTraining/SortColors-75/SortColorsSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeTraining.SortColors
+{
+    //https://leetcode.com/problems/sort-colors/
+    public class SortColorsSolution
+    {
+        public static void SortColors(int[] nums)
+        {
+            int low = 0, mid = 0, high = nums.Length - 1;
+            while (mid <= high)
+            {
+                switch (nums[mid])
+                {
+                    case 0:
+                        Swap(ref nums[low++], ref nums[mid++]);
+                        break;
+                    case 1:
+                        mid++;
+                        break;
+                    case 2:
+                        Swap(ref nums[mid], ref nums[high--]);
+                        break;
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
